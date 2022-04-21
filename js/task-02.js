adbe353d6d6c3e8b8c9fbc93489551c28877d7f4
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-04-21 19:34:26 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+const elements = ingredients
+  .map((ingredient) => `<li class="list-item">${ingredient}</li>`)
+  .join("");
+
+console.log(elements);
+
+list.innerHTML = elements;
+
