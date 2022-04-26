461a232fd2561670c765411261864e810151a586
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-04-26 11:47:43 +0200

    3 задание выполняет

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,5 @@
+const gallery = document.querySelector(".gallery");
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const markup = images
+  .map((image) => {
+   return `<li class="gallery-item"><img class="image" src="${image.url}" alt="${image.alt}" width="350", height="200"></li>`
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
